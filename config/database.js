1629cf615ed24f3651c4d5b7ea6d20f15c309ff9
wilsonstart <wilson.mesquita@canaisstart.com.br>
2023-10-03 17:10:37 -0300

    feat: landing

--- config/database.js
+++ config/database.js
@@ -7,9 +7,9 @@ module.exports = ({ env }) => ({
         client: 'postgres',
         host: env('DATABASE_HOST', '127.0.0.1'),
         port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'startv1'),
-        username: env('DATABASE_USERNAME', 'start'),
-        password: env('DATABASE_PASSWORD', 'start123'),
+        database: env('DATABASE_NAME', 'start2'),
+        username: env('DATABASE_USERNAME', 'user'),
+        password: env('DATABASE_PASSWORD', '1234'),
         ssl: env.bool('DATABASE_SSL', false),
       },
       options: {}
